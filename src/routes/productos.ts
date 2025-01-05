b68b670564e36c9397efea6c81c2ae60121ddb32
glutix <ferreyra.gluti@gmail.com>
2025-01-05 14:26:24 -0300

    asd

--- src/routes/productos.ts
+++ src/routes/productos.ts
@@ -6,7 +6,8 @@ router.get("/", (req: Request, res: Response): Response => {
   return res.send("Hola mundo");
 });
 
-
 router.get("/:id", (req: Request, res: Response): Response => {
   return res.send("Hola mundo");
 });
+
+export default router;
